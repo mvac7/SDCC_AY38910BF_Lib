d2e3d46ded146bd21b3f0d35c46608c59af7d90d
mvac7/303bcn (aka aorante) <aorante@gmail.com>
2021-07-12 12:37:19 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,251 @@
+# PSG AY-3-8910 BF MSX SDCC Library (fR3eL Project)
+
+```
+Author: mvac7
+Architecture: MSX
+Format: C Object (SDCC .rel)
+Programming language: C and Z80 assembler
+```
+
+
+---
+## Description
+
+This project is an opensource library functions for access to internal or external MSX PSG AY-3-8910. 
+
+Designed for developing MSX applications using Small Device C Compiler (SDCC).
+  
+It does not use the BIOS so it can be used to program both ROMs or MSX-DOS executables.
+  
+This library is similar to the [PSG AY-3-8910 RT](https://github.com/mvac7/SDCC_AY38910RT_Lib), but instead of writing directly to the registers, 
+it does so to a buffer that should be dumped to the AY in each VBLANK frame.
+
+It is designed to work together with the PT3player and/or ayFXplayer libraries, but you can also use it for your own or third-party player.
+
+![AY Sound System](https://raw.githubusercontent.com/mvac7/SDCC_AY38910BF_Lib/master/doc/AYlibs.png)
+
+It incorporates the SOUND function with the same behavior as the command included in MSX BASIC, 
+as well as specific functions to modify the different sound parameters of the AY. 
+
+Controls the I/O bits of register 7 (Mixer), of the internal AY.
+
+It allows to use the internal PSG of the MSX or an external one (like the one incorporated in the MEGAFLASHROM SCC+ or the Flashjacks).
+
+In the header file there is a definition of SWITCHER type, needed for the functions.
+This type uses the values "ON" or "OFF", which equals 1 and 0 respectively.
+
+Include definitions to improve the readability of your programs.
+
+In the source code (\examples), you can find applications for testing and learning purposes.
+
+![TEST AY Lib](https://raw.githubusercontent.com/mvac7/SDCC_AY38910BF_Lib/master/examples/test01_ROM/GFX/TESTAYBF_screenshot.png) 
+
+This library is part of the [MSX fR3eL Project](https://github.com/mvac7/SDCC_MSX_fR3eL).
+
+Enjoy it!                           
+
+
+---
+## History of versions
+
+* v0.9b (07 July 2021) First version (Based in AY-3-8910 RT Library)
+
+
+---
+## Requirements
+
+* Small Device C Compiler (SDCC) v4.1 > http://sdcc.sourceforge.net/
+* Hex2bin v2.5 http://hex2bin.sourceforge.net/
+
+
+
+---
+## Definitions
+
+
+### AY Type 
+
+Label | Value
+-- | --  
+AY_INTERNAL | 0
+AY_EXTERNAL | 1
+
+
+### AY Registers
+
+Label | Value | Description
+-- | -- | -- 
+AY_ToneA     |  0 | Channel A Tone Period (12 bits)
+AY_ToneB     |  2 | Channel B Tone Period (12 bits)
+AY_ToneC     |  4 | Channel C Tone Period (12 bits)
+AY_Noise     |  6 | Noise Period (5 bits)
+AY_Mixer     |  7 | Mixer
+AY_AmpA      |  8 | Channel Volume A (4 bits + B5 active Envelope)
+AY_AmpB      |  9 | Channel Volume B (4 bits + B5 active Envelope)
+AY_AmpC      | 10 | Channel Volume C (4 bits + B5 active Envelope)
+AY_EnvPeriod | 11 | Envelope Period (16 bits)
+AY_EnvShape  | 13 | Envelope Shape
+
+
+### Envelope shapes
+
+The header file defines envelope shapes in case you prefer to use it instead 
+of the numerical form:
+
+Label | Value
+-- | --  
+AY_ENV_LowerBeat     | 1 
+AY_ENV_Upper         | 4 
+AY_ENV_LeftSaw       | 8
+AY_ENV_LowerTriangle | 10 
+AY_ENV_LowerHold     | 11 
+AY_ENV_RightSaw      | 12
+AY_ENV_UpperHold     | 13
+AY_ENV_UpperTriangle | 14
+
+**Attention!** The LowerBeat shape may be produced with the values: 0, 1, 2, 3 and 9. 
+The value 1 has been assigned to *LowerBeat* as 0 can be useful in case you need to control when the envelope is triggered. In this case 0 can act as a "don't
+write the envelop" event. Remember that, every time the register 13 is written, the sound with the assigned shape is started.
+
+The Upper shape may be produced with the values: 4, 5, 6, 7 and 15.
+
+
+
+### AY channels
+
+You can use it in the functions: SetTonePeriod, SetVolume and SetChannel
+
+Label | Value
+-- | -- 
+AY_Channel_A | 0
+AY_Channel_B | 1
+AY_Channel_C | 2
+
+
+---  
+## Functions
+
+* **AY_Init**() - Initialize the buffer
+* **SOUND**(register, value) - Write into a register of PSG
+* **GetSound**(register) - Read PSG register value
+* **SetTonePeriod**(channel, period) - Set Tone Period for any channel
+* **SetNoisePeriod**(period) - Set Noise Period
+* **SetEnvelopePeriod**(period) - Set Envelope Period
+* **SetVolume**(channel, volume) - Set volume channel
+* **SetChannel**(channel, isTone, isNoise) - Mixer. Enable/disable Tone and Noise channels.
+* **SetEnvelope**(shape) - Set envelope type. Plays the sound on channels that have a volume of 16.
+* **PlayAY**() - Send data from AYREGS buffer to AY registers. (Execute on each interruption of VBLANK).
+
+
+---
+## Set Internal or External AY
+
+To indicate in which PSG the sounds are to be played, you have the **AY_TYPE** variable. 
+To select an external AY (ports 10h to 12h), like the one included in the MegaFlashROM SCC+, Flashjacks or other, you have to set the variable to 1 (or AY_EXTERNAL).
+
+```
+  AY_TYPE = AY_EXTERNAL;
+```
+
+**Attention!** When you execute the AY_Init() function, it will be updated to the default value corresponding to the internal AY.
+
+
+
+## How to use this
+
+coming soon...
+
+
+
+
+---
+## AY-3-8910 Register Table
+
+<table>
+<tr>
+<th colspan=2>Register\bit</th><th width=50>B7</th><th width=50>B6</th><th width=50>B5</th><th width=50>B4</th><th width=50>B3</th><th width=50>B2</th><th width=50>B1</th><th width=50>B0</th>
+</tr>
+<tr>
+<td>R0</td><td rowspan=2>Channel A Tone Period</td><td colspan=8 align=center>8-Bit Fine Tune A</td>
+</tr>
+<tr>
+<td>R1</td><td colspan=4></td><td colspan=4 align=center>4-Bit Coarse Tune A</td>
+</tr>
+<tr>
+<td>R2</td><td rowspan=2>Channel B Tone Period</td><td colspan=8 align=center>8-Bit Fine Tune B</td>
+</tr>
+<tr>
+<td>R3</td><td colspan=4></td><td colspan=4 align=center>4-Bit Coarse Tune B</td>
+</tr>
+<tr>
+<td>R4</td><td rowspan=2>Channel C Tone Period</td><td colspan=8 align=center>8-Bit Fine Tune C</td>
+</tr>
+<tr>
+<td>R5</td><td colspan=4></td><td colspan=4 align=center>4-Bit Coarse Tune C</td>
+</tr>
+<tr>
+<td>R6</td><td>Noise period</td><td colspan=3></td><td colspan=5 align=center>5-Bit Period control</td>
+</tr>
+<tr>
+<td rowspan=2>R7</td><td rowspan=2>Enable (bit 0=on, 1=off)</td><td colspan=2 align=center>IN/OUT</td><td colspan=3 align=center>Noise</td><td colspan=3 align=center>Tone</td></tr>
+<tr>
+<td align=center> IOB</td><td align=center>IOA</td><td align=center>C</td>
+<td align=center>B</td><td align=center>A</td><td align=center>C</td>
+<td align=center>B</td><td align=center>A</td>
+</tr>
+<tr>
+<td>R8</td><td>Channel A Amplitude</td><td colspan=2></td><td align=center>Env</td><td colspan=5 align=center>Amplitude</td>
+</tr>
+<tr>
+<td>R9</td><td>Channel B Amplitude</td><td colspan=2></td><td align=center>Env</td><td colspan=5 align=center>Amplitude</td>
+</tr>
+<tr>
+<td>R10</td><td>Channel C Amplitude</td><td colspan=2></td><td align=center>Env</td><td colspan=5 align=center>Amplitude</td>
+</tr>
+<tr>
+<td>R11</td><td rowspan=2>Envelope Period</td><td colspan=8 align=center>8-Bit Fine Tune Envelope</td>
+</tr>
+<tr>
+<td>R12</td><td colspan=8 align=center>8-Bit Coarse Tune Envelope</td>
+</tr>
+<tr>
+<td>R13</td><td>Envelope Shape/Cycle</td><td colspan=4></td><td>CONT</td><td>ATT</td><td>ALT</td><td>HOLD</td>
+</tr>
+<tr>
+<td>R14</td><td>I/O Port A Data Store</td><td colspan=8 align=center>8-Bit Parallel I/O on Port A</td>
+</tr>
+<tr>
+<td>R15</td><td>I/O Port B Data Store</td><td colspan=8 align=center>8-Bit Parallel I/O on Port B</td>
+</tr>
+</table>
+
+
+---
+## Acknowledgments
+  
+I want to give a special thanks to all those who freely share their knowledge with the MSX developer community.
+
+* Avelino Herrera > [(WEB)](http://msx.avelinoherrera.com)
+* Nerlaska > [(Blog)](http://albertodehoyonebot.blogspot.com.es)
+* Marq > [(Marq)](http://www.kameli.net/marq/)
+* MSXKun/Paxanga soft > [(WEB)](http://paxangasoft.retroinvaders.com/)
+* Fubukimaru [(gitHub)](https://github.com/Fubukimaru)
+* Andrear > [(Blog)](http://andrear.altervista.org/home/msxsoftware.php)
+* Sapphire/Z80ST > [(WEB)](http://z80st.auic.es/)
+* Fernando García > [(Curso)](http://www.z80st.es/cursos/bitvision-assembler)
+* Ramones > [(MSXblog)](https://www.msxblog.es/tutoriales-de-programacion-en-ensamblador-ramones/) - [(MSXbanzai)](http://msxbanzai.tni.nl/dev/faq.html)
+* Eric Boez > [(gitHub)](https://github.com/ericb59)
+* MSX Assembly Page > [(WEB)](http://map.grauw.nl/resources/msxbios.php)
+* Portar MSX Tech Doc > [(WEB)](https://problemkaputt.de/portar.htm)
+* MSX Resource Center > [(WEB)](http://www.msx.org/)
+* Karoshi MSX Community > [(WEB)](http://karoshi.auic.es/)
+* BlueMSX emulator >> [(WEB)](http://www.bluemsx.com/)
+* OpenMSX emulator >> [(WEB)](http://openmsx.sourceforge.net/)
+* Meisei emulator >> ?
+
+
+---
+## References
+
+* [General Instrument AY-3-8910 (wikipedia)](https://en.wikipedia.org/wiki/General_Instrument_AY-3-8910)
+* [GI AY-3-8910 Datasheet (PDF)](http://map.grauw.nl/resources/sound/generalinstrument_ay-3-8910.pdf)
---
+++ examples/test01_ROM/src/Test.c
@@ -0,0 +1,606 @@
+/* =============================================================================
+   Test1 
+
+   Version: v1.0 (07 July 2021)
+   Author: mvac7
+   Architecture: MSX
+   Format: C Object (SDCC .rel)
+   Programming language: C and Z80 assembler   
+   
+   Description:
+     Test PSG AY-3-8910 MSX SDCC Library (from fR3eL Project)
+     
+     This library is based on writing in a buffer the values of the AY 
+     registers and copying them in each VBLANK interrupt.
+     
+     It is similar to the PSG AY-3-8910 RT library  
+      
+   History of versions:
+     v1.0 (7 July 2021)
+============================================================================= */
+
+#include "../include/newTypes.h"
+#include "../include/msxSystemVariables.h"
+#include "../include/msxBIOS.h"
+
+#include "../include/interruptM1_Hooks.h"
+#include "../include/memory.h"
+#include "../include/keyboard.h"
+#include "../include/VDP_TMS9918A_MSXROM.h"
+#include "../include/VDP_PRINT.h"
+#include "../include/unRLEWBtoVRAM.h"
+
+#include "../include/AY38910BF.h"
+
+#include "../include/Test_GFX.h"
+
+
+#define PAUSE_TIME 30
+
+
+
+// Function Declarations -------------------------------------------------------
+void my_TIMI();
+
+void test1(); // test SOUND
+void test2(); // test SetTonePeriod
+void test3(); // test SetNoisePeriod
+void test4(); // test SetVolume
+void test5(); // test SetEnvelopePeriod
+void test6(); // test PlayEnvelope and SetChannel
+
+void test_Noise();
+
+void PLAY_EnvShape(char envType);
+
+void WAIT(uint cicles);
+
+void LOCATE(char x, char y) ;
+
+void VPrintNumberO(unsigned int vaddr, unsigned int value, char length, signed char offset);
+
+void Set_tone(char channel, uint period);
+void Set_Amp(char channel, char value);
+void Set_noise(char period);
+void Set_mixer(char value);
+void Set_EnvPeriod(uint period);
+void Set_EnvShape(char shape);
+void PRINTON(uint vaddr, boolean state);
+
+//const char EnvelopeValue[8]={1,4,8,10,11,12,13,14};
+const char EnvelopeIndex[16]={0,0,0,0,1,1,1,1,2,0,3,4,5,6,7,1};
+
+// constants  ------------------------------------------------------------------
+const char text01[] = "Test SDCC AY-3-8910 BF Library";
+const char text02[] = "v1.0 (7 July 2021)";
+
+const char text03[] = "                             ";
+
+// global variable definition --------------------------------------------------
+
+
+
+// Functions -------------------------------------------------------------------
+
+
+//
+void main(void)
+{
+  char result;
+  
+  AY_Init();    //Clear AY Buffer
+  
+  POKE(LINL32,32); //Width(32)
+  COLOR(BLACK,GRAY,DARK_BLUE);      
+  SCREEN(1);
+  
+  unRLEWBtoVRAM((uint) Frame_MAP,BASE5);
+  unRLEWBtoVRAM((uint) GUI_G1_TSET_COLOR,BASE6);
+  unRLEWBtoVRAM((uint) GUI_G1_TSET,BASE7);
+  
+  VPRINT(1,1,text01);
+  VPRINT(1,2,text02);
+  
+  VPRINT(1,8,"Do you want to use the");
+  VPRINT(1,9,"external AY? ([Y] for Yes)");
+  
+  LOCATE(1,10);
+  result = INKEY();
+  
+  if (result==89 || result==121) AY_TYPE=AY_EXTERNAL;
+  else AY_TYPE=AY_INTERNAL;
+  
+  unRLEWBtoVRAM((uint) GUI_MAP,BASE5+96); //show screen map
+  
+  PRINTON(0x1A6D,AY_TYPE);
+      
+  Install_TIMI(my_TIMI);  //install interruption function
+    
+  test1(); // test SOUND
+  test2(); // test SetTonePeriod
+  test3(); // test SetNoisePeriod
+  test4(); // test SetVolume
+  test5(); // test SetEnvelopePeriod
+  test6(); // test PlayEnvelope and SetChannel
+ 
+  Disable_TIMI();
+ 
+  VPRINT(1,21,">End Test                     ");
+  WAIT(250);
+ 
+  unRLEWBtoVRAM((uint) Frame_MAP,BASE5);
+  
+  WAIT(100);
+
+}
+
+
+
+// Routine for Hook TIMI (FD9Fh)
+void my_TIMI() 
+{
+  PUSH_AF;
+  
+  PlayAY(); //Dump buffer to AY
+
+  POP_AF;
+}
+
+
+
+
+
+/* =============================================================================
+ Generates a pause in the execution of n interruptions.
+ PAL: 50=1second. ; NTSC: 60=1second.
+============================================================================= */ 
+void WAIT(uint cicles)
+{
+  uint i;
+  for(i=0;i<cicles;i++) HALT;
+  return;
+}
+
+
+
+/* =============================================================================
+  LOCATE
+ 
+  Description: 
+            Moves the cursor to the specified location.
+  Input:    (char) Position X of the cursor. (0 to 31 or 79)
+            (char) Position Y of the cursor. (0 to 23)         
+  Output:   -
+============================================================================= */
+void LOCATE(char x, char y) __naked
+{
+x;y;
+__asm
+  push IX
+  ld   IX,#0
+  add  IX,SP
+  
+  ld   A,4(IX) ;x
+  inc  A       ;incrementa las posiciones para que se situen correctamente en la pantalla
+  ld   H,A
+  ld   A,5(IX) ;y
+  inc  A
+  ld   L,A   
+  call POSIT
+  
+  pop  IX
+  ret
+__endasm;
+
+}
+
+
+
+/* =============================================================================
+   VPrintNumberO
+   Prints a number at the specified position on the screen.
+   
+   Apply an offset to the tile number to be able to use a font hosted in 
+   another area of the tileset.
+   
+   Inputs:
+     [unsigned int] VRAM address in Pattern Name Table.
+     [unsigned int] number
+     [char] length
+============================================================================= */
+void VPrintNumberO(unsigned int vaddr, unsigned int value, char length, signed char offset)
+{
+  char pos=5-length;
+  char text[]="     ";
+
+  num2Dec16(value, text); 
+  
+  while (length-->0){ VPOKE(vaddr++,text[pos++]+offset);}
+  
+}
+
+
+
+
+// test SOUND
+void test1()
+{  
+ 
+  VPRINT(1,21,">Test SOUND function");  
+  VPRINT(1,22," Press a key to stop sound");
+
+  Set_tone(AY_Channel_A,1000);
+  Set_tone(AY_Channel_B,0);
+  Set_tone(AY_Channel_C,0);
+   
+  Set_noise(20);
+
+  Set_Amp(AY_Channel_A,16); //channel A envelope on
+  Set_Amp(AY_Channel_B,0);
+  Set_Amp(AY_Channel_C,0);
+  
+  Set_mixer(0B00110110);
+ 
+  Set_EnvPeriod(1000);
+  
+  Set_EnvShape(14);
+  
+  LOCATE(27,22);
+  INKEY();
+  
+  Set_Amp(AY_Channel_A,0); //silence channel A  
+  
+  WAIT(PAUSE_TIME);
+}
+
+
+
+// test SetTonePeriod
+void test2()
+{
+  uint period=0;
+  
+  VPRINT(1,21,">Test SetTonePeriod function  ");  
+  VPRINT(1,22," Press [SPACE] to next        ");
+  
+  Set_mixer(0B00111110);
+  Set_Amp(AY_Channel_A,15);
+  while(1)
+  {
+    HALT;
+    
+    if (GetKeyMatrix(8)!=255) break;
+    
+    VPrintNumberO(0x18E6,period,4,+144); //6,7
+    
+    SetTonePeriod(AY_Channel_A, period++);
+    if (period>4094) period=0;
+  
+  }  
+  Set_Amp(AY_Channel_A,0);
+  
+  WAIT(PAUSE_TIME);
+  
+  period = 0;
+  Set_mixer(0B00111101);
+  Set_Amp(AY_Channel_B,15);
+  while(1)
+  {
+    HALT;
+    
+    if (GetKeyMatrix(8)!=255) break;
+    
+    VPrintNumberO(0x18F0,period,4,+144); //16,7
+    
+    SetTonePeriod(AY_Channel_B, period++);
+    if (period>4094) period=0;
+  
+  }  
+  Set_Amp(AY_Channel_B,0);
+  
+  WAIT(PAUSE_TIME);
+  
+  period = 0;
+  Set_mixer(0B00111011);
+  Set_Amp(AY_Channel_C,15);
+  while(1)
+  {
+    HALT;
+    
+    if (GetKeyMatrix(8)!=255) break;
+    
+    VPrintNumberO(0x18FB,period,4,144); //27,7
+    
+    SetTonePeriod(AY_Channel_C, period++);
+    if (period>4094) period=0;
+  
+  }
+  Set_Amp(AY_Channel_C,0);
+  
+  WAIT(PAUSE_TIME);
+  
+}
+
+
+
+// Test SetNoisePeriod
+void test3()
+{
+  char noise=0;  
+   
+  VPRINT(1,21,">Test SetNoisePeriod function ");
+  VPRINT(1,22,text03);
+  
+  Set_mixer(0B00110111); //Mixer
+  Set_Amp(AY_Channel_A,15);
+  test_Noise();
+  
+  Set_mixer(0B00100111); //Mixer
+  Set_Amp(AY_Channel_B,15);
+  test_Noise();
+  
+  Set_mixer(0B00000111); //Mixer
+  Set_Amp(AY_Channel_C,15);
+  test_Noise();
+  
+  WAIT(PAUSE_TIME);
+  
+  Set_Amp(AY_Channel_A,0);
+  Set_Amp(AY_Channel_B,0);
+  Set_Amp(AY_Channel_C,0);   
+  
+}
+
+
+
+void test_Noise()
+{
+    char i;
+    
+    for (i=0;i<32;i++)
+    {
+        //if (GetKeyMatrix(8)!=255) break;
+    
+        VPrintNumberO(0x19CF,i,2,144); // 15,14
+    
+        SetNoisePeriod(i);
+        
+        WAIT(10);    
+    }
+}
+
+
+
+// Test SetVolume
+void test4()
+{
+  char i;
+
+  VPRINT(1,21,">Test SetVolume function     ");
+  VPRINT(1,22,text03);
+  
+  Set_noise(10);
+  
+  Set_tone(AY_Channel_A,1000);
+  Set_mixer(0B11110110);
+  for(i=0;i<16;i++)
+  {     
+    VPrintNumberO(0x1925,i,2,144); // 5,9    
+    SetVolume(AY_Channel_A,i);
+    WAIT(10);
+  }  
+  SetVolume(AY_Channel_A,0);
+  
+  WAIT(PAUSE_TIME);
+  
+
+  Set_tone(AY_Channel_B,1000);
+  Set_mixer(0B00101101);
+  for(i=0;i<16;i++)
+  { 
+    VPrintNumberO(0x192F,i,2,144); // 15,9    
+    SetVolume(AY_Channel_B,i);
+    WAIT(10);
+  }  
+  SetVolume(AY_Channel_B,0);
+  
+  WAIT(PAUSE_TIME);
+  
+  Set_tone(AY_Channel_C,1000);
+  Set_mixer(0B00011011);
+  for(i=0;i<16;i++)
+  {  
+    VPrintNumberO(0x1939,i,2,144); //25,9
+    SetVolume(AY_Channel_C,i);
+    WAIT(10);
+  }  
+  SetVolume(AY_Channel_C,0);
+  
+  WAIT(PAUSE_TIME);
+    
+}
+
+
+// Test SetEnvelopePeriod
+void test5()
+{
+  uint period=0;
+  char i;  
+  boolean isEnd=false;
+  
+  VPRINT(1,21,">Test SetEnvelopePeriod      ");
+  VPRINT(1,22," Press [SPACE] to end        ");
+  
+  //LOCATE(0,8);
+  //PRINT(" Env Period:");
+  
+  Set_tone(AY_Channel_A,1000);
+  Set_mixer(0B00111110); //Mixer
+  Set_Amp(AY_Channel_A,16);
+  Set_EnvShape(14);
+  
+  while(!isEnd)
+  {
+    VPrintNumberO(0x1A10,period,5,144); //16,16
+
+    SetEnvelopePeriod(period++);
+    
+    for(i=0;i<10;i++)
+    {
+      HALT;     
+      if (!(GetKeyMatrix(8)&Bit0)){isEnd=true;break;}
+    }    
+    //if (joytrig(0)) break;
+  }
+  
+  Set_Amp(AY_Channel_A,0);
+  
+  WAIT(PAUSE_TIME);
+  
+}
+
+
+
+
+
+// Test PlayEnvelope and SetChannel
+void test6()
+{  
+  char i;
+  //env_Index = 0;
+  //char keyvalue;
+  
+  boolean isEnd=false;
+    
+  VPRINT(1,21,">Test SetEnvelope           ");
+  VPRINT(1,22,text03);
+  
+  Set_tone(AY_Channel_A,1000);
+  Set_mixer(0B00111110); //Mixer
+  Set_Amp(AY_Channel_A,16); //Enable sound envelope
+  Set_EnvPeriod(1024);
+   
+  LOCATE(26,5);
+  
+    
+ //while(!isEnd)
+  for(i=0;i<16;i++)
+  {
+    PLAY_EnvShape(i);    
+    WAIT(100);
+  }
+  
+  Set_Amp(AY_Channel_A,0);
+  
+  WAIT(PAUSE_TIME);
+}
+
+
+
+void PLAY_EnvShape(char shape)
+{
+  char index;
+  
+  //shape = EnvelopeValue[index];
+  index = EnvelopeIndex[shape]; 
+  
+  SetEnvelope(shape);
+  
+  VPrintNumberO(0x1A30,shape,2,+144); //16,17  
+  CopyToVRAM((uint) EnvelopeShapes + (index*3), 0x1A33, 3);  // 19,17
+  
+}
+
+
+
+
+
+void Set_tone(char channel, uint period)
+{
+  uint vaddr=BASE5 + 6+(channel*10) + (7*32); //calculates the position of the numeric field
+  if (channel==2) vaddr++;  // correct the position of the third column
+  
+  VPrintNumberO(vaddr,period,4,144);  //6+(channel*10),10
+  
+  channel=channel*2;
+  SOUND(channel++,period & 0xFF);
+  SOUND(channel,(period & 0xFF00)/0xFF);
+}
+
+
+
+void Set_Amp(char channel, char value)
+{
+  boolean envState;
+  uint vaddr=BASE5 + 5+(channel*10) + (9*32); //calculates the position of the numeric field
+  
+  SOUND(8+channel,value);
+  
+  VPrintNumberO(vaddr,value,2,144); //5+(channel*10),12
+  
+  if (value&Bit4) envState=true;
+  else envState=false;
+  
+  PRINTON(0x1927+(channel*10),envState); //Envelope     (value&Bit4)>>4
+
+}
+
+
+
+void Set_noise(char period)
+{
+    VPrintNumberO(0x19CF,period,2,144); //15,14
+    SOUND(AY_Noise,period);
+}
+
+
+
+void Set_EnvPeriod(uint period)
+{
+  SOUND(11,period & 0xFF);
+  SOUND(12,(period & 0xFF00)/0xFF);
+  
+  VPrintNumberO(0x1A10,period,5,144); //16,16
+}
+
+
+
+void Set_EnvShape(char shape)
+{
+  char index = EnvelopeIndex[shape];
+    
+  SOUND(AY_EnvShape,shape); //envelope wave type
+  
+  VPrintNumberO(0x1A30,shape,2,+144); //16,17
+  CopyToVRAM((uint) EnvelopeShapes + (index*3), 0x1A33, 3); //19,17
+}
+
+
+
+void Set_mixer(char value)
+{
+    SOUND(AY_Mixer,value);
+    
+    PRINTON(0x1967,!(value&Bit0)); //tone  A
+    PRINTON(0x1971,!(value&Bit1)); //tone  B
+    PRINTON(0x197B,!(value&Bit2)); //tone  C
+    
+    PRINTON(0x1987,!(value&Bit3)); //noise A
+    PRINTON(0x1991,!(value&Bit4)); //noise B
+    PRINTON(0x199B,!(value&Bit5)); //noise C
+    
+}
+
+
+
+void PRINTON(uint vaddr, boolean state)
+{
+    char value = 221;
+    if(state==true) value=189;
+    VPOKE(vaddr++,value++);
+    VPOKE(vaddr++,value++);
+    VPOKE(vaddr,value);
+}
+
+
+
